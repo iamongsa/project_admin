866a50ac4b4ec82744818843e1c1d2ce24f91ba7
iamongsa <sudarat15fah@gmail.com>
2023-10-29 22:02:20 +0700

    เพิ่มUI หน้าแจ้งเตือน และ หน้าแดชบอร์ด

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
+import WorkOffOutlinedIcon from '@mui/icons-material/WorkOffOutlined';
+import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
+import { Chart } from "react-google-charts";
 
 function Dashboard() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["ช่างดำ", 11],
+    ["ช่างซี่", 2]
+  ];
+
+  const options = {
+    title: "Sessions"
+
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <div >
+      <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="bg-dt">
+          <div className="head-dt">งานทั้งหมด</div>
+          <div className="con-dt">
+            <div className="work-icon">
+              <WorkOutlineOutlinedIcon sx={{ fontSize: 50, color: "#2D4059" ,}}/>
+            </div>
+            <div className="dt-db"></div>
+            <h1 className="dt-num">10</h1>
+          </div>
+        </div>
+      
+
+      <div className="bg-dt">
+          <div className="head-dt">งานที่เสร็จแล้ว</div>
+          <div className="con-dt">
+            <div className="work-icon">
+              <WorkOffOutlinedIcon color="success"  sx={{ fontSize: 50 }}/>
+            </div>
+            <div className="dt-db"></div>
+            <h1 className="dt-num">5</h1>
+          </div>
+      </div>
+
+      <div className="bg-dt">
+          <div className="head-dt">งานรอดำเนินการ</div>
+          <div className="con-dt">
+            <div className="work-icon">
+              <WorkHistoryOutlinedIcon color="warning"  sx={{ fontSize: 50}}/>
+            </div>
+            <div className="dt-db"></div>
+            <h1 className="dt-num">10</h1>
+          </div>
+        </div>
+      </div>
+    
+
+      <div className="pie-db">
+      <Chart  
+      style={{    backgroundColor: 'lightgray',
+      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)'}}
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+      </div>
+      </div>
+    
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- client/src/pages/Nontiline.jsx
+++ client/src/pages/Nontiline.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import dayjs from "dayjs"
+import Axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { yellow } from "@mui/material/colors";
+import IconButton from "@mui/material/IconButton";
+import buddhistEra from "dayjs/plugin/buddhistEra";
 
 function Nontiline() {
+  dayjs.extend(buddhistEra);
+  const [taskList, setTasklist] = useState([]);
+
+  useEffect(() => {
+    getDatas();
+  });
+
+  const getDatas = () => {
+    Axios.get("http://localhost:3001/admin").then((response) => {
+      setTasklist(response.data);
+    });
+  };
+  const DateLongTH = (date) => {
+    dayjs.locale("th");
+    
+    return dayjs(date).format("DD MMMM BBBB HH MM");
+  };
+
+
   return (
-    <div>Nontiline</div>
+    <div>
+    <h1>แจ้งเตือนไลน์</h1>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    ></Box>
+    <Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">ชื่อลูกค้า</TableCell>
+              <TableCell align="center">เบอร์โทรศัพท์</TableCell>
+              <TableCell align="center">ป้ายทะเบียน</TableCell>
+              <TableCell align="center">รายละเอียด</TableCell>
+              <TableCell align="center">สถานะ</TableCell>
+              <TableCell align="center">แก้ไขรายการ</TableCell>
+              <TableCell align="center">ส่งแจ้งเตือน</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {taskList &&
+              taskList.map((val) => (
+                <TableRow
+                  key={val.order_id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {DateLongTH(val.create_order)}
+                  </TableCell>
+                  <TableCell align="center">{val.customer_name}</TableCell>
+                  <TableCell align="center">{val.plate_id}</TableCell>
+                  <TableCell align="center">{val.mech_name}</TableCell>
+                  <TableCell align="center">
+                    {val.order_description}
+                  </TableCell>
+                  <TableCell align="center">{val.repair_status}</TableCell>
+                  <TableCell align="center">
+                    <IconButton>
+                      <DeleteIcon color="error" />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell align="center">
+                    <EditIcon sx={{ color: yellow[900] }} />
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  </div>
   )
 }
 
